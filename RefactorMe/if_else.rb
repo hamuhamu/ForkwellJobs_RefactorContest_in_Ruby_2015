f2ec6e6e1440fae32ff2103e9b368cee672c8414
大橋 勇希 <y-ohhashi@y-ohhashi-mac.local>
2015-12-05 22:14:40 +0900

    振る舞いを変えずにテストを追加

---
+++ RefactorMe/if_else.rb
@@ -0,0 +1,48 @@
+class ClassificationByAge
+
+  def initialize(age)
+    0 <= age or raise
+    @age = age
+  end
+
+  def get
+    if 0 <= @age && @age <= 2
+      'baby'
+    elsif 3 <= @age && @age <= 6
+      'little child'
+    elsif 7 <= @age && @age <= 12
+      'child'
+    elsif 13 <= @age && @age <= 18
+      'youth'
+    else
+      'adult'
+    end
+  end
+
+end
+
+require 'test/unit'
+class ClassificationByAgeTest < Test::Unit::TestCase
+
+  data(
+    '0〜2才の間_下限'   => ['baby',         0],
+    '0〜2才の間_上限'   => ['baby',         2],
+    '3〜6才の間_下限'   => ['little child', 3],
+    '3〜6才の間_上限'   => ['little child', 6],
+    '7〜12才の間_下限'  => ['child',        7],
+    '7〜12才の間_上限'  => ['child',        12],
+    '13〜18才の間_下限' => ['youth',        13],
+    '13〜18才の間_上限' => ['youth',        18],
+    '19才以上_下限'     => ['adult',        19],
+  )
+  def test_年齢に応じた区分を返すこと(data)
+    expected, age = data
+    byAge = ClassificationByAge.new(age)
+    assert_equal(expected, byAge.get())
+  end
+
+  def test_年齢が負数であれば例外を返すこと
+    assert_raise(RuntimeError) { ClassificationByAge.new(-1) }
+  end
+
+end
